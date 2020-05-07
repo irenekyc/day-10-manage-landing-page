13aefd7ec1206083bbb041fa3c98f576a6966c3b
irenekyc <gugu_irene@hotmail.com>
2020-05-06 22:21:28 -0500

    fixed display issue

---
+++ docs/index.js
@@ -0,0 +1,31 @@
+const navbar = document.querySelector('nav')
+const openBtnClass= document.getElementById('open-menu-div').classList
+const closeBtnClass= document.getElementById('close-menu-div').classList
+const mobileMenuClass = document.getElementById('nav-links').classList
+
+
+navbar.addEventListener('click', (e)=>{
+    console.log(e.target.id)
+    if (e.target.id.includes("menu")){
+        if (e.target.id.includes("open") || e.target.id.includes("close")){
+            openBtnClass.toggle('hide-originally')
+            closeBtnClass.toggle('hide-originally')
+            mobileMenuClass.toggle('hide-originally')
+
+            if (e.target.id.include ("close")){
+                console.log(e.target.id)
+
+            }
+        } 
+    }
+})
+
+window.addEventListener('scroll', ()=>{
+    if (window.scrollY > 70) {
+        navbar.classList.add("navbar-white")
+      } else {
+        navbar.classList.remove("navbar-white")
+      }
+})
+
+
